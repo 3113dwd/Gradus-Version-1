4e6bc489e2c29fe5cbec426437568fe12af6e6dc
3113dwd <david.wu@my.bdsc.school.nz>
2025-08-04 18:36:27 +1200

    Initial Version 1
    Not Finalized Version 1 yet

---
+++ Iteration 1.py
@@ -0,0 +1,24 @@
+class GradeTracker:
+  def __init__(self,subject,standard,standard_number,grade):
+    self.subject = subject
+    self.standard = standard
+    self.standard_number = standard_number
+    self.grade = grade
+  
+  def add_standard():
+    pass
+    
+class CareerPlanner:
+  def __init__(self,interest):
+    self.interest = interest
+    
+
+class Frost:
+  def __init__(self,question):
+    self.question = question
+
+subject = input("Please enter the subject of the standard that you are entering")
+standard = input("Please enter the Standard you are entering")
+standard_number = input("Please enter the Standard Number of the standard")
+grade = input("please enter the grade of the standard")
+
